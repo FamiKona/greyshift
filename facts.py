dc982cdfd8dfc54c591f4cede01b49637c51f3a6
Connor Garrett <mcwvproductions@gmail.com>
2017-12-11 20:37:06 -0800

    removed empty fact tuple

--- facts.py
+++ facts.py
@@ -1,4 +1,3 @@
 facts = [('watch Bee Movie %s times!', 95.0),
-         ('climb the ladder in Metal Gear Solid 3 %s times!', 1.9),
-         ()
+         ('climb the ladder in Metal Gear Solid 3 %s times!', 1.9)
          ]
