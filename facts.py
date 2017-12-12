e728ba7c358df8e815a0110ab840641135cde768
Connor Garrett <mcwvproductions@gmail.com>
2017-12-11 20:42:03 -0800

    zelda speedrun fact

--- facts.py
+++ facts.py
@@ -1,3 +1,4 @@
 facts = [('watch Bee Movie %s times!', 95.0),
-         ('climb the ladder in Metal Gear Solid 3 %s times!', 1.9)
+         ('climb the ladder in Metal Gear Solid 3 %s times!', 1.9),
+         ('complete %s Breath Of The Wild speedruns!', 1980)
          ]
