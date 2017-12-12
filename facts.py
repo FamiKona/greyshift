287d15ec4905c8035540f2420d27ce29dc867c07
Connor Garrett <mcwvproductions@gmail.com>
2017-12-12 00:31:30 -0800

    fact rewording

--- facts.py
+++ facts.py
@@ -1,5 +1,5 @@
 facts = [('watch Bee Movie %s times!', 95.0),
          ('climb the ladder in Metal Gear Solid 3 %s times!', 1.9),
-         ('complete %s Breath Of The Wild perfect speedruns!', 1980.0),
+         ('complete %s Breath Of The Wild 100%% speedruns!', 1980.0),
          ('surrender in League of Legends %s times!', 20.0)
          ]
